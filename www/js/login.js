fca7830152a376c531cc97372437092ba8b543f3
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2017-06-30 14:51:06 -0500

    Se implementó el login en el servidor

--- www/js/login.js
+++ www/js/login.js
@@ -3,15 +3,59 @@
  */
 
 angular.module('starter.login', ['ionic'])
+    .controller('loginController', function ($state, $scope, utils, $http,
+                                             serviceHttpRequest, $q) {
+        $scope.loginData = {
+            correo: 'porfirioads@gmail.com',
+            password: 'holamundo'
+        };
 
-  .controller('loginController',function ($state, $scope) {
+        $scope.goMaps = function () {
+            $state.go('tabs.mapa')
+        };
 
-    $scope.goMaps = function () {
-      $state.go('tabs.mapa')
-    };
+        $scope.goRegistro = function () {
+            $state.go('registro')
+        };
 
-    $scope.goRegistro = function () {
-      $state.go('registro')
-    }
+        $scope.login = function () {
+            utils.showLoading();
 
-  });
+            sendLoginRequest()
+                .then(function (data) {
+                    utils.popup('info', JSON.stringify(data));
+                })
+                .catch(function (error) {
+                    utils.popup('error', JSON.stringify(error));
+                })
+                .finally(function () {
+                    utils.hideLoading();
+                });
+        };
+
+        function sendLoginRequest() {
+            var defered = $q.defer();
+            var promise = defered.promise;
+
+            var httpRequest = serviceHttpRequest.createPostHttpRequest(
+                'http://192.168.1.101:8000/login',
+                {
+                    email: $scope.loginData.correo,
+                    password: $scope.loginData.password
+                }
+            );
+
+            $http(httpRequest)
+                .then(function successCallback(response) {
+                    setTimeout(function () {
+                        defered.resolve(response.data);
+                    }, 0);
+                }, function errorCallback(response) {
+                    setTimeout(function () {
+                        defered.reject(response.data)
+                    }, 0);
+                });
+
+            return promise;
+        }
+    });
