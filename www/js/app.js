50d5dbd5e26f73e7f6d876e0b15a710405032476
Victor Hugo <victorhugo_reveles@hotmail.com>
2017-06-30 15:01:42 -0500

    Ajusté el App.js

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.mapa', 'starter.registro', 'starter.login'])
+angular.module('starter', ['ionic', 'ngCordova', 'starter.mapa', 'starter.registro', 'starter.login', 'starter.perfil'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
@@ -40,6 +40,13 @@ angular.module('starter', ['ionic', 'starter.mapa', 'starter.registro', 'starter
                 url: '/registro',
                 templateUrl: 'templates/registro.html',
                 controller: 'registroController'
+            })
+
+            .state('perfil', {
+              cache: false,
+              url: '/perfil',
+              templateUrl: 'templates/perfil.html',
+              controller: 'perfilController'
             })
 
             .state('tabs', {
